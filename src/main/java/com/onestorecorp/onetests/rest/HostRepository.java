238799cd302b2baf0f82c35a80bc7045660274ec
Dale Seo <dale.seo@gmail.com>
2017-05-11 10:29:23 +0900

    findByServiceId

--- src/main/java/com/onestorecorp/onetests/rest/HostRepository.java
+++ src/main/java/com/onestorecorp/onetests/rest/HostRepository.java
@@ -1,6 +1,8 @@
 package com.onestorecorp.onetests.rest;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 @CrossOrigin
 public interface HostRepository extends MongoRepository<Host, String> {
 
-	List<Host> findById(String id);
+	@RestResource(path="findByServiceId", rel="findByServiceId")
+	List<Host> findByServiceId(@Param("serviceId") String serviceId);
 
 }
